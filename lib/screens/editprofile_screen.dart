e8ef2cb17842cf7d400284f480e5b0856ae0ebb4
Ngoc <quangngoc.07012002@gmail.com>
2024-08-22 23:12:50 +0700

    Edit Profile Page

---
+++ lib/screens/editprofile_screen.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Profile', style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),),
+        backgroundColor: const Color(0xFF39c5c8),
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.white),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(20.0)),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 60.0,
+                      backgroundImage: NetworkImage(
+                          'https://scontent.fsgn2-3.fna.fbcdn.net/v/t39.30808-6/448249343_2254063974927591_3013120637072065076_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=u9k1rh_qdN8Q7kNvgHeWbBb&_nc_ht=scontent.fsgn2-3.fna&oh=00_AYB0lp00TWjdTXz3ijJJaz2rqg7zo3fOEQcZDN6BtJ1Q8A&oe=66CA595B'), // Replace with your own image URL
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.2),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                            ),
+                          ],
+                        ),
+                        child: IconButton(
+                          icon: const Icon(Icons.camera_alt, color: Color(0xFF39c5c8)),
+                          onPressed: () {
+                            // Handle change profile picture
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30.0),
+              _buildTextField('Name', 'Enter your name', nameController),
+              const SizedBox(height: 20.0),
+              _buildTextField('Email', 'Enter your email', emailController),
+              const SizedBox(height: 20.0),
+              _buildTextField('Phone Number', 'Enter your phone number', phoneController),
+              const SizedBox(height: 40.0),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Handle save action
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 15.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    backgroundColor: const Color(0xFF39c5c8),
+                  ),
+                  child: const Text('Save Changes', style: TextStyle(fontSize: 16.0, color: Colors.white, fontWeight: FontWeight.bold)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField(String label, String hintText, TextEditingController controller) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[700],
+          ),
+        ),
+        const SizedBox(height: 8.0),
+        TextField(
+          controller: controller,
+          decoration: InputDecoration(
+            hintText: hintText,
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              borderSide: BorderSide.none,
+            ),
+            contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              borderSide: BorderSide(color: Color(0xFF39c5c8), width: 2.0),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              borderSide: BorderSide(color: Colors.grey[300]!, width: 1.0),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}

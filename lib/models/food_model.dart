2f136822c741a08eb158a6d124b4beccff2e7bd2
Ngoc <quangngoc.07012002@gmail.com>
2024-10-29 08:49:07 +0700

    Create Model

---
+++ lib/models/food_model.dart
@@ -0,0 +1,9 @@
+class Food {
+  String id;
+  String name;
+  String imageUrl;
+  String? description;
+  bool isAvailable;
+  double price;
+  Food(this.id, this.name, this.imageUrl, this.description, this.isAvailable, this.price);
+}
---
+++ lib/models/restaurant_model.dart
@@ -0,0 +1,18 @@
+class Restaurant {
+  final String id;
+  final bool isOpen;
+  final String name;
+  final String description;
+  final String image;
+  final double rating;
+
+
+  Restaurant({
+    required this.id,
+    required this.isOpen,
+    required this.name,
+    required this.description,
+    required this.image,
+    required this.rating,
+  });
+}

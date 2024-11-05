9c809714579b5483e44a053531383df81089c557
Nguyen Quang Ngoc <quangngoc.07012002@gmail.com>
2024-11-05 23:50:30 +0700

    Food Model

--- lib/models/food_model.dart
+++ lib/models/food_model.dart
@@ -1,9 +1,9 @@
 class Food {
-  String id;
+  String? id;
   String name;
   String imageUrl;
-  String? description;
+  String description;
   bool isAvailable;
   double price;
-  Food(this.id, this.name, this.imageUrl, this.description, this.isAvailable, this.price);
+  Food(this.id, this.name, this.imageUrl, this.description, this.isAvailable, this.price, );
 }

de97e85ca4a1334f22b4c0883293ffb99c01c358
Nguyen Quang Ngoc <quangngoc.07012002@gmail.com>
2024-11-06 08:58:28 +0700

    Update Order Screen

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -1,50 +1,19 @@
+import 'package:deliveryapplication_mobile_restaurant/screens/orderdetail_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart'; // Import GetX
+import '../controllers/order_controller.dart'; // Import OrderController
+import '../models/order_model.dart';
 
-import 'orderdetail_screen.dart';
-
-class OrderPage extends StatefulWidget {
+class OrderPage extends StatelessWidget {
   const OrderPage({super.key});
 
   @override
-  _OrderPageState createState() => _OrderPageState();
-}
-
-class _OrderPageState extends State<OrderPage> {
-  List<Map<String, dynamic>> orders = [
-    {
-      'restaurantName': 'Pizza Hut',
-      'imageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyFc46glG2RnSW-wnlDZKghM-cmUlqskpIZA&s',
-      'amount': 150.0,
-      'items': [
-        {'name': 'Margherita Pizza', 'quantity': 2},
-        {'name': 'Pepperoni Pizza', 'quantity': 1},
-      ],
-    },
-    {
-      'restaurantName': 'Sushi Bar',
-      'imageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyFc46glG2RnSW-wnlDZKghM-cmUlqskpIZA&s',
-      'amount': 200.0,
-      'items': [
-        {'name': 'California Roll', 'quantity': 3},
-        {'name': 'Spicy Tuna Roll', 'quantity': 2},
-      ],
-    },
-    {
-      'restaurantName': 'Burger King',
-      'imageUrl': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyFc46glG2RnSW-wnlDZKghM-cmUlqskpIZA&s',
-      'amount': 120.0,
-      'items': [
-        {'name': 'Whopper', 'quantity': 2},
-        {'name': 'Fries', 'quantity': 1},
-      ],
-    },
-  ];
-
-  @override
   Widget build(BuildContext context) {
+    final OrderController orderController = Get.put(OrderController()); // Khởi tạo controller
 
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text('Orders', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
         backgroundColor: const Color(0xFF39c5c8),
         elevation: 0,
@@ -53,97 +22,78 @@ class _OrderPageState extends State<OrderPage> {
           borderRadius: BorderRadius.vertical(bottom: Radius.circular(20.0)),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 16.0),
-            Expanded(
-              child: ListView.builder(
-                itemCount: orders.length,
-                itemBuilder: (context, index) {
-                  final order = orders[index];
-                  return GestureDetector(
-                    onTap: () {
-                      print('Order Details: ${order}');
+      body: Obx(() {
+        if (orderController.isLoading.value) {
+          return const Center(child: CircularProgressIndicator());
+        }
 
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => OrderDetailPage(order: order),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: Row(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(12.0),
-                            child: Image.network(
-                              order['imageUrl'],
-                              width: 120,
-                              height: 120,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          const SizedBox(width: 16.0),
-                          Expanded(
-                            child: Container(
-                              padding: const EdgeInsets.all(12.0),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(12.0),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey.withOpacity(0.2),
-                                    spreadRadius: 2,
-                                    blurRadius: 5,
-                                    offset: const Offset(0, 3),
-                                  ),
-                                ],
-                              ),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        order['restaurantName'],
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16.0,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 4.0),
-                                      Text(
-                                        'Amount: \$${order['amount'].toStringAsFixed(2)}',
-                                        style: const TextStyle(
-                                          color: Colors.grey,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  const Icon(
-                                    Icons.arrow_forward_ios,
-                                    color: Colors.grey,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
+        if (orderController.orders.isEmpty) {
+          return const Center(child: Text('No orders found.'));
+        }
+
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.separated(
+            itemCount: orderController.orders.length,
+            separatorBuilder: (context, index) => const SizedBox(height: 8.0),
+            itemBuilder: (context, index) {
+              final order = orderController.orders[index];
+              return GestureDetector(
+                onTap: () {
+                  print('Order Details: ${order}');
+                  Get.to(() => OrderDetailPage(order: order,));
                 },
-              ),
-            ),
-          ],
-        ),
-      ),
+                child: Container(
+                  padding: const EdgeInsets.all(12.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.1),
+                        spreadRadius: 1,
+                        blurRadius: 3,
+                        offset: const Offset(0, 1),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      const Icon(Icons.assignment, color: Color(0xFF39c5c8)),
+                      const SizedBox(width: 10.0),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${order.customerName} - ${order.orderCode}" ?? 'Undefined' ,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16.0,
+                                color: Color(0xFF333333),
+                              ),
+                            ),
+                            const SizedBox(height: 4.0),
+                            Text(
+                              '\$${order.totalPrice?.toStringAsFixed(2)}' +
+                                  (order.orderStatus == "PENDING" ? " - Pending" : ""),
+                              style: const TextStyle(
+                                fontSize: 14.0,
+                                color: Color(0xFF39c5c8),
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      }),
     );
   }
 }

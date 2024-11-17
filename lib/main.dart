d8aa0a4f5fde2bc0d8eca321821686374ecb511e
Nguyen Quang Ngoc <quangngoc.07012002@gmail.com>
2024-11-17 23:29:04 +0700

    Update Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:deliveryapplication_mobile_restaurant/controllers/order_controller.dart';
 import 'package:deliveryapplication_mobile_restaurant/screens/homepage_screen.dart';
 import 'package:deliveryapplication_mobile_restaurant/screens/login_screen.dart';
+import 'package:deliveryapplication_mobile_restaurant/services/websocket_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'controllers/user_controller.dart';
 
 void main() {
-
+  Get.put(WebSocketService());
+  Get.put(OrderController());
   runApp(GetMaterialApp(
-    home: LoginPage(),
+    home: RestaurantDashboardPage(),
   ));
 }
